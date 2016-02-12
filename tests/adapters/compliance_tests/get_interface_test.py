fbe45a40e4e6298559fa5582b6ca683a2d661324
Félix Bouliane <fbouliane@internap.com>
2016-02-12 13:43:24 -0500

    Make get interface compare both get_interfaces and get_interface values.

--- tests/adapters/compliance_tests/get_interface_test.py
+++ tests/adapters/compliance_tests/get_interface_test.py
@@ -15,19 +15,45 @@
 from hamcrest import assert_that, is_
 
 from netman.core.objects.exceptions import UnknownInterface
+from netman.core.objects.interface_states import ON
 from tests.adapters.configured_test_case import ConfiguredTestCase
 
 
 class GetInterfaceTest(ConfiguredTestCase):
     _dev_sample = "juniper"
 
+    def setUp(self):
+        super(GetInterfaceTest, self).setUp()
+
     def test_returns_an_interface(self):
         interface = self.client.get_interface(self.test_ports[0].name)
         assert_that(interface.name, is_(self.test_ports[0].name))
+
+    def test_get_interface_and_get_interfaces_are_same(self):
+        self.client.add_vlan(1000, name="vlan1000")
+        self.client.add_vlan(2000, name="vlan2000")
+        expected = self.test_ports[0]
+
+        self.try_to.set_access_vlan(expected.name, 1000)
+        self.try_to.set_trunk_mode(expected.name)
+        self.try_to.set_interface_state(expected.name, ON)
+        self.try_to.set_interface_native_vlan(expected.name, 2000)
+
+        interface_from_single = self.client.get_interface(expected.name)
+        interfaces = [inte for inte in self.client.get_interfaces() if inte.name == expected.name]
+        interface_from_multiple = interfaces[0]
+
+        assert_that(interface_from_single.name, is_(interface_from_multiple.name))
+        assert_that(interface_from_single.port_mode, is_(interface_from_multiple.port_mode))
+        assert_that(interface_from_single.shutdown, is_(interface_from_multiple.shutdown))
+        assert_that(interface_from_single.trunk_native_vlan, is_(interface_from_multiple.trunk_native_vlan))
+        assert_that(interface_from_single.trunk_vlans, is_(interface_from_multiple.trunk_vlans))
 
     def test_fails_when_nonexistent_interface(self):
         with self.assertRaises(UnknownInterface):
             self.client.get_interface('ethernet 1/nonexistent2000')
 
     def tearDown(self):
+        self.janitor.remove_vlan(1000)
+        self.janitor.remove_vlan(2000)
         super(GetInterfaceTest, self).tearDown()
