f24c8120ba67fcd450625ea24cb08c10a09a81e6
Félix Bouliane <fbouliane@internap.com>
2016-02-18 20:48:07 -0500

    Add autoloader for test cases to prevent forgetting some compliance test.

--- tests/adapters/compliance_tests/__init__.py
+++ tests/adapters/compliance_tests/__init__.py
@@ -13,20 +13,13 @@
 # limitations under the License
 
 
-from unittest import SkipTest
-
-from tests.adapters.compliance_tests.get_interfaces_test import GetInterfacesTest
-from tests.adapters.compliance_tests.add_vlan_test import AddVlanTest
-from tests.adapters.compliance_tests.get_vlan_test import GetVlanTest
-from tests.adapters.compliance_tests.set_vlan_icmp_redirects_state_test import SetVlanIcmpRedirectsStateTest
+from tests.adapters.configured_test_case import ConfiguredTestCase
 from tests.adapters.model_list import available_models
 
-_test_classes = [
-    GetVlanTest,
-    AddVlanTest,
-    GetInterfacesTest,
-    SetVlanIcmpRedirectsStateTest
-]
+from unittest import SkipTest
+import inspect
+import sys
+import os
 
 
 def _add_tests_for_model(current_module, test_classes, specs):
@@ -56,6 +49,50 @@ def _wrap_test_method(method, test_class):
     wrapper.__name__ = method
     setattr(test_class, method, wrapper)
 
+
+def _is_class_child_of(obj, parent_class):
+    return (inspect.isclass(obj) and
+            (not obj.__name__.startswith('_')) and
+            issubclass(obj, parent_class) and
+            (inspect.getmro(obj)[0] is not parent_class))
+
+
+def _import_module(import_str):
+    __import__(import_str)
+    return sys.modules[import_str]
+
+
+def _get_classes_from_module(module_name, class_type):
+    classes = []
+    module = _import_module(module_name)
+    for obj_name in dir(module):
+        if obj_name.startswith('_'):
+            continue
+        itm = getattr(module, obj_name)
+        if _is_class_child_of(itm, class_type):
+            classes.append(itm)
+    return classes
+
+
+def _get_classes_of_type_from_directory(class_type, dir_name):
+    classes = []
+
+    for dirpath, dirnames, filenames in os.walk(dir_name):
+        relpath = os.path.relpath(dirpath, dir_name)
+        if relpath == '.':
+            relpkg = ''
+        else:
+            relpkg = '.%s' % '.'.join(relpath.split(os.sep))
+        for fname in filenames:
+            root, ext = os.path.splitext(fname)
+            if ext != '.py' or root == '__init__':
+                continue
+            module_name = "%s%s.%s" % (__package__, relpkg, root)
+            mod_classes = _get_classes_from_module(module_name, class_type)
+            classes.extend(mod_classes)
+    return classes
+
+_test_classes = _get_classes_of_type_from_directory(ConfiguredTestCase, os.path.dirname(__file__))
 
 for _specs in available_models:
     _add_tests_for_model(globals(), _test_classes, _specs)
