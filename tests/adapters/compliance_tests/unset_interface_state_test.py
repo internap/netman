d695ee37fb82cd9f1c641c016742d658f391e82e
epageau <emeric.pageau@gmail.com>
2016-03-07 13:58:46 -0500

    test naming changes test logics changes

--- tests/adapters/compliance_tests/unset_interface_state_test.py
+++ tests/adapters/compliance_tests/unset_interface_state_test.py
@@ -14,6 +14,7 @@
 
 from hamcrest import assert_that, is_
 from netman.core.objects.exceptions import UnknownInterface
+from netman.core.objects.interface_states import ON, OFF
 
 from tests.adapters.configured_test_case import ConfiguredTestCase
 
@@ -21,16 +22,19 @@ from tests.adapters.configured_test_case import ConfiguredTestCase
 class UnsetInterfaceStateTest(ConfiguredTestCase):
     _dev_sample = "juniper"
 
-    def test_unsetting_an_interface(self):
-        self.try_to.unset_interface_state(self.test_ports[0].name)
-        self.try_to.unset_interface_state(self.test_ports[1].name)
+    def test_unset_an_interface_to_default_state(self):
 
-        interface_1 = self.client.get_interface(self.test_ports[0].name)
-        interface_2 = self.client.get_interface(self.test_ports[1].name)
+        default_state = self.client.get_interface(self.test_port).shutdown
 
-        assert_that(self.test_ports[0].name, is_(interface_1.name))
-        assert_that(self.test_ports[1].name, is_(interface_2.name))
-        assert_that(interface_1.shutdown, is_(interface_2.shutdown))
+        self.try_to.set_interface_state(self.test_port, ON)
+        self.try_to.unset_interface_state(self.test_port)
+
+        assert_that(self.client.get_interface(self.test_port).shutdown, is_(default_state))
+
+        self.try_to.set_interface_state(self.test_port, OFF)
+        self.try_to.unset_interface_state(self.test_port)
+
+        assert_that(self.client.get_interface(self.test_port).shutdown, is_(default_state))
 
     def test_fails_with_unknown_interface(self):
         with self.assertRaises(UnknownInterface):
