6a81f06e615c35f966ec977b50e913e7202b1956
Martin Roy <mroy@internap.com>
2016-03-10 17:03:29 -0500

    Deprecated some members of SwitchDescriptor
    Those are remnant of an old system association and are not used in netman at all, so no reason to define them here.

--- netman/core/objects/switch_descriptor.py
+++ netman/core/objects/switch_descriptor.py
@@ -11,26 +11,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import warnings
 
 from netman.core.objects import Model
 
 
 class SwitchDescriptor(Model):
-    def __init__(self, model, hostname, username=None, password=None, port=None,
-                 default_vrf=None, default_lan_acl_in=None, default_lan_acl_out=None,
-                 trunked_interfaces=None, parking_vlan=None, netman_server=None, default_port_speed=None,
-                 vrrp_tracking_object=None):
+    def __init__(self, model, hostname, username=None, password=None, port=None, netman_server=None, **kwargs):
         self.model = model
         self.hostname = hostname
         self.username = username
         self.password = password
         self.port = port
-
-        self.default_vrf = default_vrf
-        self.default_lan_acl_in = default_lan_acl_in
-        self.default_lan_acl_out = default_lan_acl_out
-        self.default_port_speed = default_port_speed
-        self.trunked_interfaces = trunked_interfaces
-        self.parking_vlan = parking_vlan
         self.netman_server = netman_server
-        self.vrrp_tracking_object = vrrp_tracking_object
+
+        self._deprecated_values = {}
+        for m in _deprecated_members:
+            if m in kwargs:
+                setattr(self, m, kwargs.get(m))
+
+
+_deprecated_members = ["default_vrf", "default_lan_acl_in", "default_lan_acl_out", "default_port_speed",
+                       "trunked_interfaces", "parking_vlan", "vrrp_tracking_object"]
+
+
+def _deprecation_warning(member):
+    warnings.warn("Deprecated member {} is not used by Netman, define it elsewhere for your needs".format(member),
+                  DeprecationWarning)
+
+
+def _add_deprecated_property(m):
+    def get_that(switch_descriptor):
+        _deprecation_warning(m)
+        return switch_descriptor._deprecated_values.get(m)
+
+    def set_that(switch_descriptor, value):
+        _deprecation_warning(m)
+        switch_descriptor._deprecated_values[m] = value
+
+    setattr(SwitchDescriptor, m, property(get_that, set_that))
+
+
+for m in _deprecated_members:
+    _add_deprecated_property(m)
