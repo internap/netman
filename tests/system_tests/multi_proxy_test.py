630d32d2a56441f3f2af22a2b8772ede227749f1
Martin Roy <mroy@internap.com>
2016-05-04 11:44:39 -0400

    Added a test that validates the proxy chaining
    Fixes #15

---
+++ tests/system_tests/multi_proxy_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+from hamcrest import assert_that
+from hamcrest import is_
+
+from netman.adapters.threading_lock_factory import ThreadingLockFactory
+from netman.core.switch_factory import FlowControlSwitchFactory
+from tests.system_tests import NetmanTestApp, get_available_switch
+
+
+class SessionTest(unittest.TestCase):
+
+    def test_multiple_proxies_works(self):
+        with NetmanTestApp() as partial_client1, NetmanTestApp() as partial_client2:
+            factory = FlowControlSwitchFactory(None, ThreadingLockFactory())
+            switch_descriptor = get_available_switch("juniper")
+
+            client1 = partial_client1(switch_descriptor)
+            first_netman_url = "{}:{}".format(client1.host, client1.port)
+
+            client2 = partial_client2(switch_descriptor)
+            second_netman_url = "{}:{}".format(client2.host, client2.port)
+
+            switch_descriptor.netman_server = [first_netman_url, second_netman_url]
+
+            switch = factory.get_switch_by_descriptor(switch_descriptor)
+
+            with switch.transaction():
+                switch.add_vlan(1498, "one")
+
+            assert_that(client2.get("/switches/{hostname}/vlans/1498").json()["name"], is_("one"))
